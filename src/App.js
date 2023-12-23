172dbe96485546d0271768291e40811114aacfad
Arman <arman.kumaraswamy@gmail.com>
2023-12-23 18:19:14 -0500

    Add todo and empty login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Coding Club!</h1>
+
+      <h2>To-do:</h2>
+      <p>- Login page</p>
+      <p>- Register page</p>
+      <p>- Club creation page</p>
+      <p>- Club discover page</p>
+      <p>- Club card component</p>
     </div>
   );
 }
