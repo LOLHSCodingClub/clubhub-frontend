f819692d0b12c099512637c5826eb98e94558ade
Arman <arman.kumaraswamy@gmail.com>
2023-12-23 18:46:33 -0500

    Add register page

---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+// https://github.com/LOLHSCodingClub/clubhub-frontend/issues/2
+
+import React from 'react'
+
+function Register() {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
