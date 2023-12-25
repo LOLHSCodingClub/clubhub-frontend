630a1775451fcb24095eb1a3729274f05e9ab88e
Adarsh <adhusuresh2022@gmail.com>
2023-12-25 11:01:00 -0500

    moved Login.js and Register.js to ./pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import { Login } from "./pages/Login"
+import { Register } from "./pages/Register";
 
 function App() {
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,27 @@
-// https://github.com/LOLHSCodingClub/clubhub-frontend/issues/1
+import React, { useState } from "react";
 
-import React from 'react'
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [rememberme, setRememberme] = useState('');
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Forgot password?</button>
+        </div>
+    )
 }
-
-export default Login
